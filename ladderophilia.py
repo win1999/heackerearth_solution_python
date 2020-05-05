c86e2e7edd7767a27b7416afd2e1f8318f5c856f
vinit rai <37284791+win1999@users.noreply.github.com>
2020-05-05 11:56:09 +0530

    Create ladderophilia.py

---
+++ ladderophilia.py
@@ -0,0 +1,10 @@
+x=int(input())
+c="*"
+for i in range(x):
+    for i in range(2):
+        print(c.ljust(3," ")+c.rjust(2))
+        
+    for i in range(1):
+        print(c.center(1)*(5))
+for i in range(2):
+        print(c.ljust(3," ")+c.rjust(2))
