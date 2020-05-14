8d7ae8d6ad88a344f0f06fb9044ec8fbfe1de6ed
vinit rai <37284791+win1999@users.noreply.github.com>
2020-05-15 00:34:27 +0530

    Create final_destination.py

---
+++ final_destination.py
@@ -0,0 +1,15 @@
+m=input().upper()
+l=list(m)
+l.sort()
+x=0
+y=0
+for ele in l:
+    if(ele=='D'):
+        y-=1
+    elif(ele=="R"):
+        x+=1
+    elif(ele=="U"):
+        y+=1
+    else:
+        x-=1
+print(x,y)
